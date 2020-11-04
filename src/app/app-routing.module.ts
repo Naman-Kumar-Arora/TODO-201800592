1dbcebfe2657ba41208f0a89dfe57e745e5480c5
Naman Arora <namanarora146@gmail.com>
2020-11-04 14:37:23 +0530

    ROUTING COMMIT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoDetailsComponent } from './component/todo-details/todo-details.component';
+import { TodoListComponent } from './component/todo-list/todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'list',component: TodoListComponent},
+  {path: 'details',component: TodoDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
